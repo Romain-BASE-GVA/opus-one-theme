0ccb2b401b81d596db3d886bcdedc3f4f6567555
Germain <germain@swat.studio>
2022-07-15 17:29:06 +0200

    Fix: AJAX agenda

--- opus-one/assets/js/ajax.js
+++ opus-one/assets/js/ajax.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
     window_width = $(window).width();
 
     host = "https://"+window.location.hostname;
+    host = "http://localhost:10008";
 
     var processing = false;
     if ($('[data-ajax-action="next_month"]').length > 0) {
@@ -38,7 +39,7 @@ $(document).ready(function () {
                     $.ajax({
                         type: 'GET',
                         url: host + '/wp-content/themes/opus-one/ajax.php',
-                        data: {action: 'next_months', month_to_show: next_month, year_to_show: next_year, taxo_type: isTaxo, term_id: term_id}
+                        data: {action: 'next_months', month_to_show: next_month, year_to_show: next_year, isTaxo: isTaxo, term_id: term_id}
                     }).done(function (data) {
                         n = data.indexOf("one-month")
                         if (n >= 0) {
