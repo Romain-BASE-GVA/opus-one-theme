26ef9c3560bd07d313b508915d9e1e8ca3272ea1
Germain <germain@swat.studio>
2022-07-15 16:48:52 +0200

    Correction Front Loop - Agenda

--- opus-one/template-parts/categories-loop-home.php
+++ opus-one/template-parts/categories-loop-home.php
@@ -34,11 +34,10 @@
                     
                     <div class="category__bottom">
                         <a href="<?= get_term_link($term, 'taxonomy-types'); ?>" class="see-more"><span>+</span></a>
-                        <?php var_dump($term); ?>
-                        <?php $nextEvents = get_show_from_category_nb_types(10, $term->term_id); ?>
+                        <?php $nextEvents = get_show_from_category_nb_types($term->term_id, 10); ?>
                         
                         <div class="shortly">
-                            <span class="shortly__title">Prochainement</span>
+                            <span class="shortly__title"><?= __('Prochainement', 'opus-one') ?></span>
                             <div class="marquee3k shortly__marquee" data-speed="1" data-pausable="true">
                                 <div>
                                 <?php foreach ($nextEvents as $show): ?>
